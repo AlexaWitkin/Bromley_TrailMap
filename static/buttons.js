72dc0ee0ccabd637be6ca76bd72842f94cc7b834
alexawitkin <anwitkin@uvm.org>
2024-07-18 22:40:54 -0400

    added easy trail buttons to html, js, and css - need to add lift buttons to js and css

--- static/buttons.js
+++ static/buttons.js
@@ -3,10 +3,11 @@ buttons.js
 TrailMapInterface V1.0
  */
 
-var lock_button = $("#lock_button");
-lock_button.click(function() {
+/* Open */
+var open_button_chase_it = $("#open_button_chase_it");
+open_button_chase_it.click(function() {
     $.ajax({
-        url: "/lock_door",
+        url: "/open_button_chase_it",
         type: "post",
         success: function(response) {
             console.log(response);
@@ -14,10 +15,10 @@ lock_button.click(function() {
     });
 });
 
-var unlock_button = $("#unlock_button");
-unlock_button.click(function() {
+var open_button_crackerjack = $("#open_button_crackerjack");
+open_button_crackerjack.click(function() {
     $.ajax({
-        url: "/unlock_door",
+        url: "/open_button_crackerjack",
         type: "post",
         success: function(response) {
             console.log(response);
@@ -25,10 +26,10 @@ unlock_button.click(function() {
     });
 });
 
-var light_on_button = $("#light_on_button");
-light_on_button.click(function() {
+var open_button_learning_zone = $("#open_button_learning_zone");
+open_button_learning_zone.click(function() {
     $.ajax({
-        url: "/light_on",
+        url: "/open_button_learning_zone",
         type: "post",
         success: function(response) {
             console.log(response);
@@ -36,10 +37,430 @@ light_on_button.click(function() {
     });
 });
 
-var light_off_button = $("#light_off_button");
-light_off_button.click(function() {
+var open_button_lower_boulevard = $("#open_button_lower_boulevard");
+open_button_lower_boulevard.click(function() {
     $.ajax({
-        url: "/light_off",
+        url: "/open_button_lower_boulevard",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_lower_east_meadow = $("#open_button_lower_east_meadow");
+open_button_lower_east_meadow.click(function() {
+    $.ajax({
+        url: "/open_button_lower_east_meadow",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_lower_thruway = $("#open_button_lower_thruway");
+open_button_lower_thruway.click(function() {
+    $.ajax({
+        url: "/open_button_lower_thruway",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_plaza = $("#open_button_plaza");
+open_button_plaza.click(function() {
+    $.ajax({
+        url: "/open_button_plaza",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_proutys_past = $("#open_button_proutys_past");
+open_button_proutys_past.click(function() {
+    $.ajax({
+        url: "/open_button_proutys_past",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_run_around_1 = $("#open_button_run_around_1");
+open_button_run_around_1.click(function() {
+    $.ajax({
+        url: "/open_button_run_around_1",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_run_around_2 = $("#open_button_run_around_2");
+open_button_run_around_2.click(function() {
+    $.ajax({
+        url: "/open_button_run_around_2",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_run_around_3 = $("#open_button_run_around_3");
+open_button_run_around_3.click(function() {
+    $.ajax({
+        url: "/open_button_run_around_3",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_run_around_4 = $("#open_button_run_around_4");
+open_button_run_around_4.click(function() {
+    $.ajax({
+        url: "/open_button_run_around_4",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_school_slope = $("#open_button_school_slope");
+open_button_school_slope.click(function() {
+    $.ajax({
+        url: "/open_button_school_slope",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_woods_run = $("#open_button_woods_run");
+open_button_woods_run.click(function() {
+    $.ajax({
+        url: "/open_button_woods_run",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+/* Delayed */
+var delayed_button_chase_it = $("#delayed_button_chase_it");
+delayed_button_chase_it.click(function() {
+    $.ajax({
+        url: "/delayed_button_chase_it",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_crackerjack = $("#delayed_button_crackerjack");
+delayed_button_crackerjack.click(function() {
+    $.ajax({
+        url: "/delayed_button_crackerjack",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_learning_zone = $("#delayed_button_learning_zone");
+delayed_button_learning_zone.click(function() {
+    $.ajax({
+        url: "/delayed_button_learning_zone",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_lower_boulevard = $("#delayed_button_lower_boulevard");
+delayed_button_lower_boulevard.click(function() {
+    $.ajax({
+        url: "/delayed_button_lower_boulevard",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_lower_east_meadow = $("#delayed_button_lower_east_meadow");
+delayed_button_lower_east_meadow.click(function() {
+    $.ajax({
+        url: "/delayed_button_lower_east_meadow",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_lower_thruway = $("#delayed_button_lower_thruway");
+delayed_button_lower_thruway.click(function() {
+    $.ajax({
+        url: "/delayed_button_lower_thruway",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_plaza = $("#delayed_button_plaza");
+delayed_button_plaza.click(function() {
+    $.ajax({
+        url: "/delayed_button_plaza",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_proutys_past = $("#delayed_button_proutys_past");
+delayed_button_proutys_past.click(function() {
+    $.ajax({
+        url: "/delayed_button_proutys_past",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_run_around_1 = $("#delayed_button_run_around_1");
+delayed_button_run_around_1.click(function() {
+    $.ajax({
+        url: "/delayed_button_run_around_1",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_run_around_2 = $("#delayed_button_run_around_2");
+delayed_button_run_around_2.click(function() {
+    $.ajax({
+        url: "/delayed_button_run_around_2",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_run_around_3 = $("#delayed_button_run_around_3");
+delayed_button_run_around_3.click(function() {
+    $.ajax({
+        url: "/delayed_button_run_around_3",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_run_around_4 = $("#delayed_button_run_around_4");
+delayed_button_run_around_4.click(function() {
+    $.ajax({
+        url: "/delayed_button_run_around_4",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_school_slope = $("#delayed_button_school_slope");
+delayed_button_school_slope.click(function() {
+    $.ajax({
+        url: "/delayed_button_school_slope",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_woods_run = $("#delayed_button_woods_run");
+delayed_button_woods_run.click(function() {
+    $.ajax({
+        url: "/delayed_button_woods_run",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+/* Closed */
+var closed_button_chase_it = $("#closed_button_chase_it");
+closed_button_chase_it.click(function() {
+    $.ajax({
+        url: "/closed_button_chase_it",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_crackerjack = $("#closed_button_crackerjack");
+closed_button_crackerjack.click(function() {
+    $.ajax({
+        url: "/closed_button_crackerjack",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_learning_zone = $("#closed_button_learning_zone");
+closed_button_learning_zone.click(function() {
+    $.ajax({
+        url: "/closed_button_learning_zone",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_lower_boulevard = $("#closed_button_lower_boulevard");
+closed_button_lower_boulevard.click(function() {
+    $.ajax({
+        url: "/closed_button_lower_boulevard",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_lower_east_meadow = $("#closed_button_lower_east_meadow");
+closed_button_lower_east_meadow.click(function() {
+    $.ajax({
+        url: "/closed_button_lower_east_meadow",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_lower_thruway = $("#closed_button_lower_thruway");
+closed_button_lower_thruway.click(function() {
+    $.ajax({
+        url: "/closed_button_lower_thruway",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_plaza = $("#closed_button_plaza");
+closed_button_plaza.click(function() {
+    $.ajax({
+        url: "/closed_button_plaza",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_proutys_past = $("#closed_button_proutys_past");
+closed_button_proutys_past.click(function() {
+    $.ajax({
+        url: "/closed_button_proutys_past",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_run_around_1 = $("#closed_button_run_around_1");
+closed_button_run_around_1.click(function() {
+    $.ajax({
+        url: "/closed_button_run_around_1",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_run_around_2 = $("#closed_button_run_around_2");
+closed_button_run_around_2.click(function() {
+    $.ajax({
+        url: "/closed_button_run_around_2",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_run_around_3 = $("#closed_button_run_around_3");
+closed_button_run_around_3.click(function() {
+    $.ajax({
+        url: "/closed_button_run_around_3",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_run_around_4 = $("#closed_button_run_around_4");
+closed_button_run_around_4.click(function() {
+    $.ajax({
+        url: "/closed_button_run_around_4",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_school_slope = $("#closed_button_school_slope");
+closed_button_school_slope.click(function() {
+    $.ajax({
+        url: "/closed_button_school_slope",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_woods_run = $("#closed_button_woods_run");
+closed_button_woods_run.click(function() {
+    $.ajax({
+        url: "/closed_button_woods_run",
         type: "post",
         success: function(response) {
             console.log(response);
