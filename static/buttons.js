0768255d6638ec54330f6b5e3dedc37120239284
alexawitkin <anwitkin@uvm.org>
2024-07-21 09:53:32 -0400

    added lift buttons to js and css

--- static/buttons.js
+++ static/buttons.js
@@ -3,7 +3,9 @@ buttons.js
 TrailMapInterface V1.0
  */
 
-/* Open */
+/*
+Open Easiest Trails
+*/
 var open_button_chase_it = $("#open_button_chase_it");
 open_button_chase_it.click(function() {
     $.ajax({
@@ -158,7 +160,9 @@ open_button_woods_run.click(function() {
     });
 });
 
-/* Delayed */
+/*
+Delayed Easiest Trails
+*/
 var delayed_button_chase_it = $("#delayed_button_chase_it");
 delayed_button_chase_it.click(function() {
     $.ajax({
@@ -313,7 +317,9 @@ delayed_button_woods_run.click(function() {
     });
 });
 
-/* Closed */
+/*
+Closed Easiest Trails
+*/
 var closed_button_chase_it = $("#closed_button_chase_it");
 closed_button_chase_it.click(function() {
     $.ajax({
@@ -461,6 +467,312 @@ var closed_button_woods_run = $("#closed_button_woods_run");
 closed_button_woods_run.click(function() {
     $.ajax({
         url: "/closed_button_woods_run",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+/*
+Open Lifts
+*/
+var open_button_sun_mountain_express = $("#open_button_sun_mountain_express");
+open_button_sun_mountain_express.click(function() {
+    $.ajax({
+        url: "/open_button_sun_mountain_express",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_plaza_chairlift = $("#open_button_plaza_chairlift");
+open_button_plaza_chairlift.click(function() {
+    $.ajax({
+        url: "/open_button_plaza_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_sun_chairlift = $("#open_button_sun_chairlift");
+open_button_sun_chairlift.click(function() {
+    $.ajax({
+        url: "/open_button_sun_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_alpine_chairlift = $("#open_button_alpine_chairlift");
+open_button_alpine_chairlift.click(function() {
+    $.ajax({
+        url: "/open_button_alpine_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_east_meadow_chairlift = $("#open_button_east_meadow_chairlift");
+open_button_east_meadow_chairlift.click(function() {
+    $.ajax({
+        url: "/open_button_east_meadow_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_star_carpet = $("#open_button_star_carpet");
+open_button_star_carpet.click(function() {
+    $.ajax({
+        url: "/open_button_star_carpet",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_blue_ribbon_quad = $("#open_button_blue_ribbon_quad");
+open_button_blue_ribbon_quad.click(function() {
+    $.ajax({
+        url: "/open_button_blue_ribbon_quad",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_lords_prayer_tbar = $("#open_button_lords_prayer_tbar");
+open_button_lords_prayer_tbar.click(function() {
+    $.ajax({
+        url: "/open_button_lords_prayer_tbar",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var open_button_kids_carpet = $("#open_button_kids_carpet");
+open_button_kids_carpet.click(function() {
+    $.ajax({
+        url: "/open_button_kids_carpet",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+/*
+Delayed Lifts
+*/
+var delayed_button_sun_mountain_express = $("#delayed_button_sun_mountain_express");
+delayed_button_sun_mountain_express.click(function() {
+    $.ajax({
+        url: "/delayed_button_sun_mountain_express",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_plaza_chairlift = $("#delayed_button_plaza_chairlift");
+delayed_button_plaza_chairlift.click(function() {
+    $.ajax({
+        url: "/delayed_button_plaza_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_sun_chairlift = $("#delayed_button_sun_chairlift");
+delayed_button_sun_chairlift.click(function() {
+    $.ajax({
+        url: "/delayed_button_sun_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_alpine_chairlift = $("#delayed_button_alpine_chairlift");
+delayed_button_alpine_chairlift.click(function() {
+    $.ajax({
+        url: "/delayed_button_alpine_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_east_meadow_chairlift = $("#delayed_button_east_meadow_chairlift");
+delayed_button_east_meadow_chairlift.click(function() {
+    $.ajax({
+        url: "/delayed_button_east_meadow_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_star_carpet = $("#delayed_button_star_carpet");
+delayed_button_star_carpet.click(function() {
+    $.ajax({
+        url: "/delayed_button_star_carpet",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_blue_ribbon_quad = $("#delayed_button_blue_ribbon_quad");
+delayed_button_blue_ribbon_quad.click(function() {
+    $.ajax({
+        url: "/delayed_button_blue_ribbon_quad",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_lords_prayer_tbar = $("#delayed_button_lords_prayer_tbar");
+delayed_button_lords_prayer_tbar.click(function() {
+    $.ajax({
+        url: "/delayed_button_lords_prayer_tbar",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var delayed_button_kids_carpet = $("#delayed_button_kids_carpet");
+delayed_button_kids_carpet.click(function() {
+    $.ajax({
+        url: "/delayed_button_kids_carpet",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+/*
+Closed Lifts
+*/
+var closed_button_sun_mountain_express = $("#closed_button_sun_mountain_express");
+closed_button_sun_mountain_express.click(function() {
+    $.ajax({
+        url: "/closed_button_sun_mountain_express",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_plaza_chairlift = $("#closed_button_plaza_chairlift");
+closed_button_plaza_chairlift.click(function() {
+    $.ajax({
+        url: "/closed_button_plaza_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_sun_chairlift = $("#closed_button_sun_chairlift");
+closed_button_sun_chairlift.click(function() {
+    $.ajax({
+        url: "/closed_button_sun_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_alpine_chairlift = $("#closed_button_alpine_chairlift");
+closed_button_alpine_chairlift.click(function() {
+    $.ajax({
+        url: "/closed_button_alpine_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_east_meadow_chairlift = $("#closed_button_east_meadow_chairlift");
+closed_button_east_meadow_chairlift.click(function() {
+    $.ajax({
+        url: "/closed_button_east_meadow_chairlift",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_star_carpet = $("#closed_button_star_carpet");
+closed_button_star_carpet.click(function() {
+    $.ajax({
+        url: "/closed_button_star_carpet",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_blue_ribbon_quad = $("#closed_button_blue_ribbon_quad");
+closed_button_blue_ribbon_quad.click(function() {
+    $.ajax({
+        url: "/closed_button_blue_ribbon_quad",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_lords_prayer_tbar = $("#closed_button_lords_prayer_tbar");
+closed_button_lords_prayer_tbar.click(function() {
+    $.ajax({
+        url: "/closed_button_lords_prayer_tbar",
+        type: "post",
+        success: function(response) {
+            console.log(response);
+        }
+    });
+});
+
+var closed_button_kids_carpet = $("#closed_button_kids_carpet");
+closed_button_kids_carpet.click(function() {
+    $.ajax({
+        url: "/closed_button_kids_carpet",
         type: "post",
         success: function(response) {
             console.log(response);
